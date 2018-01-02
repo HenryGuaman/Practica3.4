2033182385aadcd44e3936c8284f6aa7286eec10
Abuelito <Abuelito@192.168.1.7>
2018-01-01 20:36:54 -0500

    En la clase GestionDato hemos hecho los metodos para insertar en las tablas las respetivas clases de Modelo, A continuancion se haran las tablas de Relacion.

--- practica_2_4/src/controlador/GestionDato.java
+++ practica_2_4/src/controlador/GestionDato.java
@@ -5,10 +5,106 @@
  */
 package controlador;
 
+import Modelo.Aeropuerto;
+import Modelo.Avion;
+import Modelo.Boleto;
+import Modelo.Pasajero;
+import Modelo.ProgramaVuelo;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author Eduardo
  */
 public class GestionDato {
-    
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("practica_2_4PU");
+
+    public boolean insertAeropuerto(Aeropuerto a) {
+
+        boolean retorno = false;
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(a);
+            em.getTransaction().commit();
+            retorno = true;
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+        return retorno;
+
+    }
+
+    public boolean insertAvion(Avion a) {
+
+        boolean retorno = false;
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(a);
+            em.getTransaction().commit();
+            retorno = true;
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+        return retorno;
+
+    }
+
+    public boolean insertBoleto(Boleto b) {
+
+        boolean retorno = false;
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(b);
+            em.getTransaction().commit();
+            retorno = true;
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+        return retorno;
+
+    }
+
+    public boolean insertPasajero(Pasajero p) {
+
+        boolean retorno = false;
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(p);
+            em.getTransaction().commit();
+            retorno = true;
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+        return retorno;
+
+    }
+
+    public boolean insertProgramaVuelo(ProgramaVuelo pV) {
+
+        boolean retorno = false;
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(pV);
+            em.getTransaction().commit();
+            retorno = true;
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+        return retorno;
+
+    }
+
 }
