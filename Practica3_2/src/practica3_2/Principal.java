4493ee0e023d6975b660a707df483ff944ce7209
SVAIO <SVAIO@DESKTOP-IG5E1LP>
2018-01-10 21:44:08 -0500

    Implementacion en la clase principal

--- Practica3_2/src/practica3_2/Principal.java
+++ Practica3_2/src/practica3_2/Principal.java
@@ -9,6 +9,7 @@ import Modelo.Casa;
 import Modelo.Propietario;
 import Controlador.Diccionario;
 import Controlador.Lista;
+
 /**
  *
  * @author SVAIO
@@ -20,17 +21,17 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-Propietario p = new Propietario(123, "Juan Perez");
+        Propietario p = new Propietario(123, "Juan Perez");
         Propietario p1 = new Propietario(456, "Marco Rene");
         Propietario p2 = new Propietario(2341, "Tito Durazno  ");
         Propietario p3 = new Propietario(223423, "Henry Guaman");
 
         Diccionario d = new Diccionario();
-      //  d.agregarElemento(1, p);
+        //  d.agregarElemento(1, p);
         //d.agregarElemento(2, p1);
         //d.agregarElemento(3, p2);
         //d.agregarElemento(4, p3);
-     
+
         //d.imprimir();
         /*
         System.out.println("DESPUES DE ELIMINAR ");
@@ -39,27 +40,27 @@ Propietario p = new Propietario(123, "Juan Perez");
         d.imprimir();
         System.out.println("Recuperar elemento");
         d.recuperarElemento(3);*/
-        
-       // System.out.println("Recuperar toda la lista");
+        // System.out.println("Recuperar toda la lista");
         //d.recuperarElementos();
-        
         System.out.println("Elementos de Casa");
         Casa c = new Casa(1, p);
         Casa c1 = new Casa(2, p1);
-        Casa c2 = new Casa(3, p);
-        Casa c3 = new Casa(4, p1);
-        
-        Lista list=new Lista();
+        Casa c2 = new Casa(3, p2);
+        Casa c3 = new Casa(4, p3);
+
+        Lista list = new Lista();
         list.agregarElemento(c);
         list.agregarElemento(c1);
         list.agregarElemento(c2);
         list.agregarElemento(c3);
-        
+
+        list.imprimir();
+
+        list.eliminarElemento(c1);
+
         list.imprimir();
         
-       list.eliminarElemento(c1);
-        
-       list.imprimir();
+        list.recuperarElemento(2);
     }
 
 }
